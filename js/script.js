e97c3c30d1e3e6b27eaaf2732e2bbbfd37223322
Lemuel Pereira <lemuelpjr@gmail.com>
2019-07-10 11:46:13 -0300

    add nav menu background animation for small screens

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 var navIcon = document.getElementById('nav-icon');
 var navMenu = document.querySelector('nav');
+var navMenuBg = document.querySelector('.menu-bg');
 
 navIcon.addEventListener('click', function() {
   // Rotate bars to create an X in the navigation menu icon
-  navIcon.classList.toggle('change');
+  navIcon.classList.toggle('change-nav-icon');
   // Toggle navigation menu
   navMenu.classList.toggle('toggle-menu');
+  // Toggle navigation menu background
+  navMenuBg.classList.toggle('change-bg');
 });
