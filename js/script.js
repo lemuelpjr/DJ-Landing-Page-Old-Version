b5630f6e8b953b4bb074abbb8480f279b039d8bd
Lemuel Pereira <lemuelpjr@gmail.com>
2019-07-10 10:53:03 -0300

    display menu when nav icon is clicked

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Rotate bars to create an X in the navigation menu icon
 var navIcon = document.getElementById('nav-icon');
+var navMenu = document.querySelector('nav');
+
 navIcon.addEventListener('click', function() {
+  // Rotate bars to create an X in the navigation menu icon
   navIcon.classList.toggle('change');
+  // Toggle navigation menu
+  navMenu.classList.toggle('toggle-menu');
 });
